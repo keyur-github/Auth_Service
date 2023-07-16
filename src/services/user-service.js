ad36881cf511a94bfc77432c079416f9d9cffcc1
Keyur Panchal <keyur.np@somaiya.edu>
2023-07-16 18:52:38 +0530

    Signup logic with passwrod encryption done

---
+++ src/services/user-service.js
@@ -0,0 +1,34 @@
+const UserRepository = require('../repository/user-repository');
+
+
+class UserService {
+
+    constructor() {
+        this.userRepository = new UserRepository();
+    }
+
+    async create(data) {
+        try {
+            const user = this.userRepository.create(data);
+            return user;  
+        } 
+        catch (error) {
+            console.log("Something went wrong in User service layer");
+            throw error;
+        }
+    }
+
+    async destroy(userId) {
+        try {
+            const response = await this.userRepository.destroy(userId);
+            return response; 
+        } 
+        catch (error) {
+            console.log("Something went wrong in User service layer");
+            throw error;
+        }
+    }
+
+}
+
+module.exports = UserService;
