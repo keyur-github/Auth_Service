a1ab518fc4dde2a5f278e0ea188a0428d101770e
Keyur Panchal <keyur.np@somaiya.edu>
2023-07-18 20:00:32 +0530

    Added roles and user_roles through table

--- src/config/serverConfig.js
+++ src/config/serverConfig.js
@@ -6,5 +6,5 @@ dotenv.config();
 module.exports = {
     PORT: process.env.PORT,
     SALT: bcrypt.genSaltSync(10),
-    JWT_KEY: process.env.JWT_KEY
+    JWT_KEY: process.env.JWT_KEY,
 }
--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 
-const {PORT} = require('./config/serverConfig');
+const { PORT } = require('./config/serverConfig');
 const apiRoutes = require('./routes/index');
+const db = require('./models/index');
 
 // const { User } = require('./models/index');
 // const bcrypt = require('bcrypt');
@@ -15,26 +16,15 @@ const prepareAndStartServer = () => {
 
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
-
     app.use('/api', apiRoutes);
 
 
     app.listen(PORT, async () => {
 
         console.log(`Server started on port ${PORT}`);
-        // const incomingPass = '123456';
-        // const user = await User.findByPk(3);
-        // const response = bcrypt.compareSync(incomingPass, user.password);
-        // console.log(response);
-        // const userRepository = new UserRepository();
-        // const user = await userRepository.getById(3);
-        // console.log(user);
-        // const userService = new UserService();
-        // const token = await userService.createToken({email: 'sanket@admin.com', id: 2});
-        // console.log("New token is:", token);
-        // const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InNhbmtldEBhZG1pbi5jb20iLCJpZCI6MiwiaWF0IjoxNjg5NTY3MjI1LCJleHAiOjE2ODk1NjcyNTV9.d82X7a3Kut1Sox7cQBMXxznL3lWdwWP-CM-cuYK0Uf0";
-        // const response = await userService.verifyToken(token);
-        // console.log(response);
+        if(process.env.DB_SYNC) {
+            db.sequelize.sync({alter: true});
+        }
 
     });
 
